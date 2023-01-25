bb67fee3b2629c2f1862fb94fba8bb39bceaaea3
TipsyCanoe <121973172+TipsyCanoe@users.noreply.github.com>
2023-01-25 11:35:23 -0800

    code for in lab

--- Mitsy.java
+++ Mitsy.java
@@ -7,59 +7,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-// import java.util.ArrayList; 
-
+import java.util.ArrayList; 
 public class Mitsy {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
+  
     // load data from file into array
-    int line = 0;
-    double weight_total = 0;
-    double[] dog_weights = new double[line];
-
-    
-    try{
-      Scanner scanner = new Scanner(new File("terrier_weights.txt"));
-      while(scanner.hasNextLine()){
-        line++;
-      }
-      scanner.close();
-      System.out.println(line);
-    }
-    catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
     try {
-      int i = 0;
       Scanner scanner = new Scanner(new File("terrier_weights.txt"));
-      while(scanner.hasNextLine()){
-        dog_weights[i] = scanner.nextDouble();
-        weight_total = scanner.nextDouble();
-        i++;
-      }
-      System.out.println(dog_weights);
-      
+      public static void ArrayList<Double> dog_weights(double n){
+          ArrayList<Double> dog_weights = new ArrayList<Double>();
+          for(i = 0: i <100; i++) {
+              double weight = scanner.nextDouble();
+              dog_weights.add(weight);
+    }
+    return dog_weights;
+}
       // work here
      
       scanner.close();
-
-      
-
     } catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-  Scanner keyboard_input = new Scanner(System.in);
-  System.out.println("What is Misty's wight?");
-  String their_wight = keyboard_input.nextLine();
-  keyboard_input.close();
-  // compute average here
-  double avg = weight_total / line;
-  System.out.println(their_wight);
-  System.out.println(avg);
-  
-
-
-	}
-
+e.printStackTrace();
+}
+double sum = 0
+for(int j = 0; i < dog_weights.size(); j++){
+  sum += dog_weights.get(j);
+  double avg = sum / dog_weights.length();
+}
+return avg;
+    // compute average here
+}
 }
